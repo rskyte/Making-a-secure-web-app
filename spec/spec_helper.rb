e209ddb6e45fbc8069a6e7631d9dd95a3032d0fa
Rhys Kyte <rskyte@gmail.com>
2018-02-05 15:51:51 +0000

    changed testing port to 3001

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'capybara/rspec'
 require_relative '../server.rb'
-Thread.new{ run_server }
+Thread.new{ run_server(3001) }
 Capybara.default_driver = :selenium
 
-Capybara.app_host = 'http://localhost:3000'
+Capybara.app_host = 'http://localhost:3001'
