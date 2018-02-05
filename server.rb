190479cba9b04f6acccc16aa9aba000f370180ee
Tim Williamson <timwilliamson1337@gmail.com>
2018-02-05 16:22:13 +0000

    Adds ability to visit a second page

--- server.rb
+++ server.rb
@@ -5,10 +5,12 @@ def run_server(port = 3000)
   p 'in server'
   while(true) do
     Thread.start(server.accept) do |socket|
-      a = socket.gets.chomp
-      p a
+      request = socket.gets.chomp
+      p request
       puts "- Got request -"
-      response = "Hello World!"
+      resource = get_resource_from request
+      p resource
+      response = "Hello World!" unless response = find_resource(resource)
       http_response = build_http_response(response)
       socket.print http_response
       socket.close
@@ -24,6 +26,14 @@ def build_http_response(response)
     "Connection: close\r\n" +
     "\r\n" +
     response
+end
+
+def find_resource resource
+  "Sign up" if resource == "/users/new"
+end
+
+def get_resource_from request
+  request.split(" ")[1]
 end
 
 run_server if "server.rb" == $0
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -6,4 +6,9 @@ feature "Server functions" do
     expect(page).to_not have_content "I don't exist"
   end
 
+  scenario "Displays a sign up page" do
+  	visit '/users/new'
+  	expect(page).to have_content 'Sign up'
+  end
+
 end
