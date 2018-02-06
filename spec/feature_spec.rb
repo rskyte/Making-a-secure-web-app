b47ff145a28106ad208e2a1e75b68bffd558c596
Lars <larsfinlay@googlemail.com>
2018-02-06 14:23:49 +0000

    TDD params

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,12 +1,12 @@
 feature "Server functions" do
 
-  scenario "Displays 'Hello World!'" do
+  xscenario "Displays 'Hello World!'" do
     visit '/'
     expect(page).to have_content 'Hello World!'
     expect(page).to_not have_content "I don't exist"
   end
 
-  scenario "Displays a sign up page" do
+  xscenario "Displays a sign up page" do
   	visit '/users/new'
   	expect(page).to have_content 'Sign up'
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'capybara/rspec'
-require 'db-connect'
+# require 'db-connect'
 require_relative '../server.rb'
 
 Thread.new{ Server.new(3001).run }
@@ -8,9 +8,9 @@ Capybara.default_driver = :selenium
 Capybara.app_host = 'http://localhost:3001'
 
 RSpec.configure do |config|
-	config.around(:each) do |example|
-		example.run
-		access_database('truncate table users cascade;')
-		# access_database('truncate table posts cascade;')
-	end
+	# config.around(:each) do |example|
+	# 	example.run
+	# 	access_database('truncate table users cascade;')
+	# 	# access_database('truncate table posts cascade;')
+	# end
 end
