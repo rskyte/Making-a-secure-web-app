5c7de81a5ed743ce83e840c87b966d70178b09df
Rhys Kyte <rskyte@gmail.com>
2018-02-06 14:04:06 +0000

    added create db file

--- lib/db-connect.rb
+++ lib/db-connect.rb
@@ -5,17 +5,18 @@ def create_table name
 end
 
 def insert table, command
-  
+
 end
 
-def access_database command, &block 
-  connection = PG.connect(dbname: 'hackapp_test') 
+def access_database command, &block
+  connection = PG.connect(dbname: 'hackapp_test')
   connection.exec(command) do |result|
   	yield(result) if block
   end
 end
 
-def migrate
+def create_db
+  conn = PG.connect(dbname: 'postgres')
+  conn.exec("CREATE DATABASE hackapp_test")
 	access_database("create table users(id serial, username varchar(255));")
-end	
-
+end
