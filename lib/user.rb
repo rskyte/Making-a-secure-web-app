b4cb74545fc6b5e7d1368ee05fb60461f5280621
Rhys Kyte <rskyte@gmail.com>
2018-02-07 16:42:40 +0000

    updates model to include attr accessors

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,8 @@ require_relative 'db-model'
 
 class User
   include DBModel
-
+  attr_reader :id
+  attr_accessor :username
   def initialize(params)
     @id = params[:id]
     @username = params[:username]
