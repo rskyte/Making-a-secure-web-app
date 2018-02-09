8403fb9b479a97164a2e2c4c7ccfe686b8827a3b
Tim Williamson <timwilliamson1337@gmail.com>
2018-02-09 17:10:45 +0000

    closes connections to prevent too many clients error (bugfix)

--- lib/db-connect.rb
+++ lib/db-connect.rb
@@ -3,9 +3,11 @@ require 'pg'
 class DBConnect
   def self.access_database command, &block
     connection = PG.connect(dbname: "hackapp_#{ENV['DB_ENV']}")
-    connection.exec(command) do |result|
+    result =connection.exec(command) do |result|
     	yield(result) if block
     end
+    connection.close
+    return result
   end
 
   def self.create_db(name = 'test')
@@ -14,10 +16,12 @@ class DBConnect
     conn = PG.connect(dbname: "hackapp_#{name}")
   	conn.exec("create table users(id serial, username varchar(255));")
     conn.exec("create table posts(id serial, content text, user_id integer);")
+    conn.close
   end
 
   def self.clear(name = 'test')
     conn = PG.connect(dbname: 'postgres')
     conn.exec("DROP DATABASE IF EXISTS hackapp_#{name};")
+    conn.close
   end
 end
