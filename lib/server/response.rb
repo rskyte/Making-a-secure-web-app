f0f130b0d5abaa7bf3109a96682216acb70c0e9e
Tim Williamson <timwilliamson1337@gmail.com>
2018-02-12 10:59:45 +0000

    fixed errors in getting files

--- lib/server/response.rb
+++ lib/server/response.rb
@@ -6,8 +6,8 @@ class Response
 	end
 	def build
 		res =  "HTTP/1.1 #{@params[:code]}\r\n" +
-      "Connection: close\r\n" +
-      "Content-type: text/html\r\n"
+       "Connection: close\r\n"# +
+      # "Content-type: text/html\r\n"
     res += "Location: #{@params[:location]}\r\n" if @params[:location]
 		res += "Set-Cookie: #{@params[:cookie]}\r\n" if @params[:cookie]
     res +="\r\n" +
