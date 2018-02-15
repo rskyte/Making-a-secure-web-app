f819b5926dbe849e704ebcb97218bb7cd9392e41
Tim Williamson <timwilliamson1337@gmail.com>
2018-02-15 12:18:40 +0000

    improved formatting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,19 +11,19 @@ Thread.new{ Server.new(3001, App).run }
 Capybara.default_driver = :selenium
 Capybara.app_host = 'http://localhost:3001'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-])
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::Console]
+)
 SimpleCov.start do
   add_filter "spec/hacks/hacks_spec.rb"
 end
 
 RSpec.configure do |config|
-  config.filter_run_excluding :ignore => true
+  config.filter_run_excluding ignore: true
 
   config.around(:each) do |example|
-	example.run
-	DBConnect.access_database('truncate table users cascade;')
-	DBConnect.access_database('truncate table posts cascade;')
+    example.run
+    DBConnect.access_database('truncate table users cascade;')
+    DBConnect.access_database('truncate table posts cascade;')
   end
 end
