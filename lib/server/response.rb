0892fae57959e09bc2a53924fe4877d8d51091dc
James Lizamore <jwlizamore@gmail.com>
2018-02-15 12:58:34 +0000

    Fixed response.rb styling

--- lib/server/response.rb
+++ lib/server/response.rb
@@ -1,20 +1,20 @@
 class Response
-  def initialize params
+  def initialize(params)
     @params = params
-    @params[:code] ||= "200 OK"
-    @params[:text] ||= ""
+    @params[:code] ||= '200 OK'
+    @params[:text] ||= ''
   end
+
   def build
     res =  "HTTP/1.1 #{@params[:code]}\r\n" +
-       "Connection: close\r\n"# +
-      # "Content-type: text/html\r\n"
+           "Connection: close\r\n" # +
+           # "Content-type: text/html\r\n"
     res += "Location: #{@params[:location]}\r\n" if @params[:location]
     res += "Set-Cookie: #{@params[:cookie]}\r\n" if @params[:cookie]
-    res +="\r\n" +
-      @params[:text]
+    res + "\r\n" + @params[:text]
   end
 
   private
-  attr_reader :text, :code
 
+  attr_reader :text, :code
 end
