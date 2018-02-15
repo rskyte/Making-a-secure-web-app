727551dcd97b1d1c7138472bcfed08548b005628
Tim Williamson <timwilliamson1337@gmail.com>
2018-02-15 12:59:04 +0000

    more linting

--- spec/server/middleware_spec.rb
+++ spec/server/middleware_spec.rb
@@ -1,27 +1,25 @@
 describe Middleware do
-
-  let(:request) { double(get_method: 'TRY', get_location: '/test')}
-  let(:request2) { double(get_method: 'TRY', get_location: '/test/test2/test3')}
-  let(:request3) { double(get_method: 'TRY', get_location: '/not_a_real_route')}
-  let(:response_class) {double(:response_class, new: response)}
-  let(:response) {double(:response)}
+  let(:request) { double(get_method: 'TRY', get_location: '/test') }
+  let(:request2) { double(get_method: 'TRY', get_location: '/test/test2/test3') }
+  let(:request3) { double(get_method: 'TRY', get_location: '/not_a_real_route') }
+  let(:response_class) { double(:response_class, new: response) }
+  let(:response) { double(:response) }
   let(:app_class) { double(new: app_instance) }
   let(:app_instance) { double(try_test: 'Test passed', try_test_test2_test3: 'Test 2 passed') }
   subject(:middleware) { described_class.new app_class, response_class }
 
   it 'should build a suitable name for a request given' do
-    expect(response_class).to receive(:new).with({text: 'Test passed'})
+    expect(response_class).to receive(:new).with({ text: 'Test passed' })
     subject.get_response(request)
   end
 
   it 'should handle multiple slashes' do
-    expect(response_class).to receive(:new).with({text: 'Test 2 passed'})
+    expect(response_class).to receive(:new).with({ text: 'Test 2 passed' })
     subject.get_response(request2)
   end
 
   it 'correctly returns 404 errors' do
-    expect(response_class).to receive(:new).with({text: '<h1>404 Error</h1><br>Page not found', code: '404 Not Found'})
+    expect(response_class).to receive(:new).with({ text: '<h1>404 Error</h1><br>Page not found', code: '404 Not Found' })
     subject.get_response(request3)
   end
-
 end
--- spec/server/response_spec.rb
+++ spec/server/response_spec.rb
@@ -1,10 +1,8 @@
 describe Response do
-
-	describe '#build' do	
-		it 'builds a 200 OK response' do
-			params = {text: 'text'}
-			expect(described_class.new(params).build).to eq "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\ntext"
-		end
-	end
-
+  describe '#build' do	
+    it 'builds a 200 OK response' do
+      params = { text: 'text' }
+      expect(described_class.new(params).build).to eq "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\ntext"
+    end
+  end
 end
